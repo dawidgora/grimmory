41e330c45a35ffc8a971f9fd95f16db3e6175cb5
Dawid Góra <dawidgora@icloud.com>
2026-08-25 19:30:39 +0200

    fix(ui): localize Lubimyczytac lock labels

--- frontend/src/app/features/book/components/book-browser/lock-unlock-metadata-dialog/lock-unlock-metadata-dialog.component.ts
+++ frontend/src/app/features/book/components/book-browser/lock-unlock-metadata-dialog/lock-unlock-metadata-dialog.component.ts
@@ -73,8 +73,8 @@ export class LockUnlockMetadataDialogComponent implements OnInit {
     hardcoverBookIdLocked: 'Hardcover Book ID',
     googleIdLocked: 'Google ID',
     comicvineIdLocked: 'Comicvine ID',
-    lubimyczytacIdLocked: 'Lubimyczytać ID',
-    lubimyczytacRatingLocked: 'Lubimyczytać ★',
+    lubimyczytacIdLocked: this.t.translate('metadata.lubimyczytacId'),
+    lubimyczytacRatingLocked: this.t.translate('metadata.lubimyczytacRating'),
     ranobedbIdLocked: 'Ranobedb ID',
     ranobedbRatingLocked: 'Ranobedb ★',
   };
